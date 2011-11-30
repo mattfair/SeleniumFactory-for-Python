2136b6eebd10ea8620577640b0d81939802da859
Matt Fair <matt.fair@gmail.com>
2011-11-29 23:20:35 -0700

    created a simple test for Selenium 1 and 2

--- simple_test.py
+++ simple_test.py
@@ -7,7 +7,7 @@ from SeleniumFactory import *
 
 class testParseSauceURL(unittest.TestCase):
 	def setUp(self):
-		self.url = "sauce-ondemand:?username=foobar&access-key=1234-5678-9102-3456&job-name=Trading Engine - Trading Engine Trunk - UAT - Firefox Linux-81&os=Linux&browser=firefox&browser-version=7&firefox-profile-url=&max-duration=300&idle-timeout=90&user-extensions-url="
+		self.url = "sauce-ondemand:?username=foobar&access-key=1234-5678-9102-3456&job-name=simple test&os=Linux&browser=firefox&browser-version=7&firefox-profile-url=&max-duration=300&idle-timeout=90&user-extensions-url="
 
 	def test_parse(self):
 		parse = ParseSauceURL(self.url)	
@@ -24,17 +24,27 @@ class testParseSauceURL(unittest.TestCase):
 
 
 
-class test(unittest.TestCase):
+class testSelenium2(unittest.TestCase):
     def setUp(self):
     	self.browser = SeleniumFactory().createWebDriver()
     					
     def test_get(self):
-      	#self.browser.get("http://amazon.com")
-      	self.browser.get("http://localhost:8080")
-        assert "Tomcat" in self.browser.title()
+      	self.browser.get("http://amazon.com")
+        assert "Amazon.com" in self.browser.title()
     
     def tearDown(self):
     	self.browser.close()
+ 
+class testSelenium1(unittest.TestCase):
+    def setUp(self):
+    	self.browser = SeleniumFactory().create()
+    					
+    def test_open(self):
+      	self.browser.open("http://amazon.com")
+        assert "Amazon.com" in self.browser.get_title()
+    
+    def tearDown(self):
+    	self.browser.stop()
     	
 
 if __name__ == "__main__":
