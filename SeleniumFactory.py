187f41a105a197a6afc24bea77d3e54b6d030b59
Matt Fair <matt.fair@gmail.com>
2011-11-30 00:41:58 -0700

    call start

--- SeleniumFactory.py
+++ SeleniumFactory.py
@@ -37,6 +37,7 @@ class SeleniumFactory:
         if 'SELENIUM_DRIVER' in os.environ and  'SELENIUM_HOST' in os.environ and 'SELENIUM_PORT' in os.environ:
             parse = ParseSauceURL(os.environ["SELENIUM_DRIVER"])  
             driver = selenium(os.environ['SELENIUM_HOST'], os.environ['SELENIUM_PORT'], parse.toJSON(), startingUrl)
+            driver.start()
             driver.set_timeout(90000)
             
             return driver
